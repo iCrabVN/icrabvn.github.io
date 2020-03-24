32215615386883aa817d8e5013aaec0c44016561
DoPo07 <61135598+DoPo07@users.noreply.github.com>
2020-03-24 17:18:53 +0700

    Uploads File

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	// Facebook Button
+	$('#facebook-btn').click(function(event) {
+		confirm("Comming Soon !");
+	});
+	// Report Button
+	$('#report-btn').click(function(event) {
+		window.location = "https://forms.gle/seREHaH7q9EPCW5G7";
+	});
+});
