71e57052f75b89f734b29177fbc3a12c02a200f2
DoPo07 <61135598+DoPo07@users.noreply.github.com>
2020-04-03 17:30:59 +0700

    Upload Files

---
+++ js/btn-css.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // Donate with btc
+    $('#donate-with-btc').addClass('btc-btn');
+    // Facebook btn
+    $('#facebook-btn').addClass('facebook-button');
+});
--- js/btn-process.js
+++ js/btn-process.js
@@ -7,6 +7,4 @@ $(document).ready(function() {
 	$('#report-btn').click(function() {
 		window.location = "https://forms.gle/seREHaH7q9EPCW5G7";
 	});
-	// Add CSS to Facebok button
-	$('#facebook-btn').addClass('facebook-button');
 });
